1111450cf157ff04e90b039f86c3a99e441dd2e4
inovailab <danielima123@hotmail.com>
2026-03-17 16:51:18 -0300

    mudança no run_click nome da pasta

--- run_click.sh
+++ run_click.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-LOG="/home/felipe/faturamento_academia/cron_rpa.log"
+LOG="/home/felipe/daniel-faturamento_academia/cron_rpa.log"
 
 echo "========================================" >> "$LOG"
 echo "[DEBUG] Iniciando run_click.sh $(date -u '+%Y-%m-%d %H:%M:%S')" >> "$LOG"
 
 # Ambiente básico para CRON
 export HOME=/home/felipe
-export PATH="/home/felipe/faturamento_academia/venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export PATH="/home/felipe/daniel-faturamento_academia/venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export DISPLAY=:99
 export PLAYWRIGHT_BROWSERS_PATH="/home/felipe/.cache/ms-playwright"
 
@@ -16,7 +16,7 @@ echo "[DEBUG] Python usado pelo CRON:" >> "$LOG"
 which python >> "$LOG"
 python --version >> "$LOG"
 
-cd /home/felipe/faturamento_academia || exit 1
+cd /home/felipe/daniel-faturamento_academia || exit 1
 
 echo "[DEBUG] Rodando click_automatico.py..." >> "$LOG"
 python click_automatico.py >> "$LOG" 2>&1
